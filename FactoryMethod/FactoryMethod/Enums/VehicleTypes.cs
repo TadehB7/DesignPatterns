559fcb78bcd444cd17c7547da6a9335f21b6196e
Tadeh Boghosian <tadehb83@gmail.com>
2023-05-16 14:52:24 -0700

    Add FactoryMethod

---
+++ FactoryMethod/FactoryMethod/Enums/VehicleTypes.cs
@@ -0,0 +1,12 @@
+﻿namespace FactoryMethod.Enums
+{
+    public enum VehicleTypes
+    {
+        Unknown = 0,
+        Car,
+        Motorcycle,
+        Bicycle,
+        Truck,
+        Other
+    }
+}
